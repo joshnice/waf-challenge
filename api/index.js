04c508e80e608031d720eef10e6fcc5c7c48e0c6
Josh Nice <joshliamnice@gmail.com>
2023-12-03 08:49:58 +0000

    Added headers

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ exports.handler = async function (event, context) {
   const responseBody = { hello: "from waf-challenge" };
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
     body: JSON.stringify(responseBody),
   };
 };
